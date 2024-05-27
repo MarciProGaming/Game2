bd4365888ea68299c048a676403565350f61c842
Polyák Marcell <polyakmarci.pm.24@gmail.com>
2024-05-27 16:57:37 +0200

    Fixing error while choosing character.

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ welcome()
 
 # Variables
 
-global charactertype
 charactertype = "No character choosen"
 
 # Modifiers
@@ -33,11 +32,6 @@ barbmagicdmg = 0
 barblongdmg = 0
 barbhealth = 0
 barbshield = 0
-modclosedmg = 1
-modmagicdmg = 1
-modlongdmg = 1
-modhealth = 1
-modshield = 1
 
 # Archer
 archclosedmg = 0
@@ -45,11 +39,6 @@ archmagicdmg = 0
 archlongdmg = 0
 archhealth = 0
 archshield = 0
-modclosedmg = 1
-modmagicdmg = 1
-modlongdmg = 1
-modhealth = 1
-modshield = 1
 
 # Medic
 medclosedmg = 0
@@ -57,18 +46,13 @@ medmagicdmg = 0
 medlongdmg = 0
 medhealth = 0
 medshield = 0
-modclosedmg = 1
-modmagicdmg = 1
-modlongdmg = 1
-modhealth = 1
-modshield = 1
 
 # Modifiers
-modclosedmg = 1
-modmagicdmg = 1
-modlongdmg = 1
-modhealth = 1
-modshield = 1
+modclosedmg = 0
+modmagicdmg = 0
+modlongdmg = 0
+modhealth = 0
+modshield = 0
 
 
 def characters():
@@ -77,25 +61,27 @@ def characters():
     print("\n---------------------------------------------------")
     print("The first one is the Barbarian. A close combat character, so you don't need to avoid enemies.")
     print("- Health: 350")
-    print("- +20% damage with close combat weapons")
-    print("- -25% damage with magical weapons")
-    print("- -50% damage with long range weapons")
+    print("- +20 damage with close combat weapons")
+    print("- -25 damage with magical weapons")
+    print("- -50 damage with long-range weapons")
     print("\nSpecial abilities: Tornado (Spins around the enemies causing huge damage to them. 120s cooldown)")
     time.sleep(10)
+    print("\n---------------------------------------------------")
     print("\n")
     print("The second one is the Archer. A ranged combat character, so you don't need to get too close to the enemies.")
     print("- Health: 350")
-    print("- -60% damage with close combat weapons")
-    print("- +10% damage with magical weapons")
-    print("- +20% damage with long range weapons")
+    print("- -60 damage with close combat weapons")
+    print("- +10 damage with magical weapons")
+    print("- +20 damage with long-range weapons")
     print("\nSpecial abilities: Arrow shower (Arrows rain from the sky, multiple enemies. 90s cooldown)")
     time.sleep(10)
+    print("\n---------------------------------------------------")
     print("\n")
     print("The third one is the Medic. Mixed range of combat, a bit weaker, but can heal itself.")
     print("- Health: 220")
-    print("- -25% damage with close combat weapons")
-    print("- +20% damage with magical weapons")
-    print("- -5% damage with long range weapons")
+    print("- -25 damage with close combat weapons")
+    print("- +20 damage with magical weapons")
+    print("- -5 damage with long-range weapons")
     print("\nSpecial abilities: Heal (Heals 20% of the characters full health. 75s cooldown)")
     time.sleep(10)
 
@@ -104,32 +90,48 @@ characters()
 
 
 def choosecharacter():
-    print("Please type in the name of the character you'd like to play with. (Example: Medic)")
-    charactertype = input('---->')
-    if charactertype == "Barbarian":
-        health = barbhealth
-        closedmg = barbclosedmg
-        longdmg = barblongdmg
-        magicdmg = barbmagicdmg
-        shield = barbshield
-    elif charactertype == "Medic":
-        health = medhealth
-        closedmg = medclosedmg
-        longdmg = medlongdmg
-        magicdmg = medmagicdmg
-        shield = medshield
-    elif charactertype == "Archer":
-        health = archhealth
-        closedmg = archclosedmg
-        longdmg = archlongdmg
-        magicdmg = archmagicdmg
-        shield = archshield
-    else:
-        print("You've entered the name incorrectly, please try again!")
-    print(f"You've choosen the character '{str(charactertype)}'")
-    print("\n---------------------------------------------------")
-    print("")
-    print("")
+    global health, modhealth, shield, longdmg, closedmg, magicdmg, charactertype
+    while True:
+        try:
+            print("Please type in the name of the character you'd like to play with. (Example: Medic)")
+            charactertype = input('---->')
+            if charactertype == "Barbarian" or charactertype == "Medic" or charactertype == "Archer":
+                break
+
+        except:
+            print("Error in 'choosecharacter()'")
+
+        if charactertype == "Barbarian":
+            health = barbhealth
+            closedmg = barbclosedmg
+            longdmg = barblongdmg
+            magicdmg = barbmagicdmg
+            shield = barbshield
+
+        elif charactertype == "Medic":
+            health = medhealth
+            closedmg = medclosedmg
+            longdmg = medlongdmg
+            magicdmg = medmagicdmg
+            shield = medshield
+
+        elif charactertype == "Archer":
+            health = archhealth
+            closedmg = archclosedmg
+            longdmg = archlongdmg
+            magicdmg = archmagicdmg
+            shield = archshield
+
+        else:
+            print("You've entered the name incorrectly, please try again!")
+
+            print(f"You've chosen the character '{str(charactertype)}'")
+            print("\n---------------------------------------------------")
+            print(f"Health: {str(health)}/{str(health + modhealth)}")
+            print(f"Shield: {str(shield)}/{str(shield + modshield)}")
+            print(f"Long-range damage: {str(longdmg)}/{str(longdmg + modlongdmg)}")
+            print(f"Close-range damage: {str(closedmg)}/{str(closedmg + modclosedmg)}")
+            print(f"Magic damage: {str(magicdmg)}/{str(magicdmg + modmagicdmg)}")
 
 
 choosecharacter()
